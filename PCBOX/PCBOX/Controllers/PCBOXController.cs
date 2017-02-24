1371233dbe17390278bc8efa1bc3767f8422d7a3
2h 2h <2h@ereglada.saleszar.com>
2017-02-24 08:30:08 +0100

    Acceso a datos Signed-off-by: Jaime Lopez Viartola

---
+++ PCBOX/PCBOX/Controllers/PCBOXController.cs
@@ -0,0 +1,39 @@
+﻿using PCBOX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PCBOX.Controllers
+{
+    public class PCBOXController : Controller
+    {
+        // GET: PCBOX
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Index2()
+        {
+            PCBOXEntities db = new PCBOXEntities();
+            ViewBag.Categorias = new SelectList(db.Categoria, "CategoriaId", "nomCategoria");
+            ViewBag.SubCategorias = new SelectList(db.Subcategoria, "SubcategoriaId", "nomSubcategoria");
+            ViewBag.Productos = new SelectList(db.Producto, "ProductoId", "nomProducto");
+            ViewBag.Marcas = new SelectList(db.Marca, "MarcaId", "nomMarca");
+            return View(db.Producto.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index2(int Productos)
+        {
+            PCBOXEntities db = new PCBOXEntities();
+            ViewBag.Categorias = new SelectList(db.Categoria, "CategoriaId", "nomCategoria");
+            ViewBag.SubCategorias = new SelectList(db.Subcategoria, "SubcategoriaId", "nomSubcategoria");
+            ViewBag.Productos = new SelectList(db.Producto, "ProductoId", "nomProducto");
+            ViewBag.Marcas = new SelectList(db.Marca, "MarcaId", "nomMarca");
+            return View(db.Producto.Where(x => x.ProductoId == Productos).ToList());
+        }
+    }
+}
---
+++ PCBOX/PCBOX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PCBOX.Startup))]
+namespace PCBOX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
